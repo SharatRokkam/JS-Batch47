439a3674a5bf381bb072a6b0c7811098d0e8c434
SharatRokkam <rokkamsharath1@gmail.com>
2025-05-26 13:00:55 +0530

    intro to js

---
+++ Introduction/script.js
@@ -0,0 +1,46 @@
+// console.log(26 - 3);
+// alert("Form submitted successfully");
+// prompt("enter your age");
+
+var a = 10;
+let myName = "Sharat";
+let isLoggedIn = false;
+
+// Rules :
+// 1. if, else, string,etc : inbuilt words should not be used..
+// 2. variable name should never start with number and symbl
+// let #= 10;
+// 3. First Letter
+// let _myname = 10;
+// let $something;
+// let name = "aflaj";
+
+// let myname = "something";
+
+// let, var and const
+// var is a function scope variable and it can redeclared and reinitialized
+
+var myname; //declaration
+
+myname = "something"; //initialization
+
+var myname = "nothing";
+
+console.log(myname);
+
+//let is block scope variable and it can reinitialized and cannot be redeclared;
+
+let isStudent = true;
+
+isStudent = false;
+console.log(isStudent);
+
+//const is block scope variable and it can not be reinitialized and not redeclared;
+// it has to be declared and initialized on the same line
+
+// const fruit;
+// fruit = "mango"
+
+// const fruit = "mango";
+
+// const fruit = "banana";
