e24316abc23dd1f8ff325c6df0e48eca3288f69a
SharatRokkam <rokkamsharath1@gmail.com>
2025-06-12 13:27:26 +0530

    array and methods

---
+++ 10_array/array.js
@@ -0,0 +1,77 @@
+// let fruits = ["banana", "apple", "mango", "grapes"];
+
+// console.log(fruits);
+
+// let empty = []
+
+// console.log(veggies.push("potatos"));
+// console.log(veggies);
+// console.log(veggies.pop());
+// console.log(veggies);
+
+// console.log(veggies.pop(true));
+// console.log(veggies)
+
+// let veggies = new Array(1, true, undefined, null);
+
+// let newArray = veggies.unshift("tomatoes");
+
+// console.log(newArray);
+
+// console.log(veggies.shift());
+// console.log(veggies);
+
+// console.log(veggies.slice(1, 3));
+
+// splice and slice
+
+// splice - add, remove and replace
+
+// let modified = bikes.splice(2, 0, "java", "royal enfield");
+// console.log(modified);
+// console.log(bikes);
+
+// Add/Remove: push, pop, unshift, shift, slice and splice
+
+// Search : lastindexOf, indexOf, find, some, and every
+
+let bikes = [
+  "duke",
+  "honda",
+  "bmw",
+  "hero",
+  "yamaha",
+  "triumph",
+  "hero",
+  { specialBike: "harley davidson" },
+];
+
+// array of objects
+
+// console.log(bikes.indexOf("triumph"));
+// console.log(bikes.lastIndexOf("hero"));
+// console.log(num.find((n) => n > 50)); //first occurrence of the element
+// console.log(num.some((n) => n > 20));
+// console.log(num.every((n) => n > 20));
+
+// let newNumbers = [7, 2, 5, 6, 34, 23, 1223];
+// let num = [[10, 1], [20], [30, 50]];
+// console.log(num.flat());
+// let newArray = [[[[[[[[[[]]]]]]]]]];
+// console.log(newArray);
+
+// console.log(newNumbers.concat(num));
+
+// console.log(newNumbers.sort((a, b) => b - a));
+
+// console.log(newNumbers.reverse());
+
+// match and matchAll
+
+// regular expression
+
+// const fruit = " apple banana apple apple";
+// console.log(fruit.length);
+
+// console.log(fruit.match(/apple/));
+// console.log(fruit.match(/apple/g));
